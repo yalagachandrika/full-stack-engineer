b30d028040a48a4d69ea4fda362b6f6b8799c7aa
yalagachandrika <chandrikayalaga2003@gmail.com>
2026-07-27 22:29:59 +0530

    let,const

--- variables/script.js
+++ variables/script.js
@@ -1,2 +1,6 @@
-let name=" chandrika";
-console.log("hello " + name);
+const name= "chandrika";
+let age= 25;
+const college= "ABC College";
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("College: " + college);
